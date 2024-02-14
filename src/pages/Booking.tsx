21bd2ba7ff8fd4a54ffbab00ae0ce59d605ba517
maijanp <maijapapazi@gmail.com>
2024-02-14 15:28:21 +0100

    booking-dummy

--- src/pages/Booking.tsx
+++ src/pages/Booking.tsx
@@ -1,9 +1,11 @@
+import { BookTable } from "../components/booking/BookTable"
 import { Header } from "../components/header/Header"
 import HeaderImg from '../images/booking_headerimage1.png'
 export const Booking = () => {
     return (
     <>
     <Header title="Boka bord" imageUrl={HeaderImg} />
+    <BookTable></BookTable>
     </>
     )
     
