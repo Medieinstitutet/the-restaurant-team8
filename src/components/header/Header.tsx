2d2da87b1a0cda301a7b100026d07770b122122e
maijanp <maijapapazi@gmail.com>
2024-02-21 00:36:03 +0100

    Revert "Merge branch 'styling-responsive'"
    This reverts commit 8393e0f742c7b62f064cb3270e901c33d8daaa47, reversing
    changes made to 198b948c33c6e0fad4be9d03da982404d4527cd2.

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,19 +1,16 @@
 import './Header.scss'
 
 interface IHeaderProps {
-  title: string;
-  imageUrl: string;
+    title: string,
+    imageUrl: string
 }
 
-export const Header = ({ title, imageUrl }: IHeaderProps) => {
-  return (
-    <>
-      <header>
+export const Header = ({title, imageUrl}: IHeaderProps) => {
+
+    return <>
+    <header>
         <h1>{title}</h1>
-        <div className="headerImgContainer">
-          <img src={imageUrl} alt="header-bakgrund" />
-        </div>
-      </header>
+        <img src={imageUrl} alt="header-bakgrund" />
+    </header>
     </>
-  );
-};
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,27 +2,21 @@ import { Header } from "../components/header/Header";
 import HeaderImage from "../../src/images/home_headerimg.png";
 import { TextSection } from "../components/textSection/TextSection";
 import RestaurantImage from "../images/restaurant.png";
-import { OpeningHours } from "../components/openingHours/OpeningHours";
-import { NavLink } from "react-router-dom";
-import { Booking } from "./Booking";
 export const Home = () => {
   return (
     <>
       <Header title="Välkommen till Lotus Lounge" imageUrl={HeaderImage} />
-  
-        <TextSection
-          headline="Smaker från Indien, hjärtat i Stockholm"
-          text="Upptäck en värld av autentiska,
-          kryddstarka rätter i en atmosfär där tradition möter modernitet.
-          Hos oss blir varje måltid ett minne att bära med sig.
-          Förena smak, kultur och gemenskap vid vårt bord.
-          Upplev det bästa av Indien, en tugga i taget."
-        />
-        <div className="textAndImage">
-          <div className="imgContainer">
-          <img src={RestaurantImage} alt="restaurang" />
-          </div>
-        <OpeningHours/>
+      <TextSection
+        headline="Smaker från Indien, hjärtat i Stockholm"
+        text="Upptäck en värld av autentiska, 
+        kryddstarka rätter i en atmosfär där tradition möter modernitet. 
+        Hos oss blir varje måltid ett minne att bära med sig. 
+        Förena smak, kultur och gemenskap vid vårt bord. 
+        Upplev det bästa av Indien, en tugga i taget."
+      />
+
+      <div style={{width: "80vw", margin: "auto"}}>
+        <img style= {{width: "100%"}} src={RestaurantImage} alt="restaurang" />
       </div>
 
     </>
