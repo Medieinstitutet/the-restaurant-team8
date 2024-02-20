a6839f907e8fbba9872cea706af580ad67f6fefd
Albin <Albin.Svensson@medieinstitutet.se>
2024-02-20 12:15:08 +0100

    bugfix type

--- src/models/CustomerClass.ts
+++ src/models/CustomerClass.ts
@@ -1,9 +1,9 @@
 export class CustomerClass {
-    constructor(
-        public _id: string,
-     public   name: string,
-     public   lastname: string,
-      public  email: string,
-     public   phone: string,
-    ) {}
+  constructor(
+    public _id: string,
+    public name: string,
+    public lastname: string,
+    public email: string,
+    public phone: number
+  ) {}
 }
