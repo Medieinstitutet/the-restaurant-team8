96e8dfb3a2229448542ca90f93fd92e09c0247d7
maijanp <maijapapazi@gmail.com>
2024-02-21 02:11:27 +0100

    menu page made responsive

--- src/components/menu/FoodMenu.tsx
+++ src/components/menu/FoodMenu.tsx
@@ -1,21 +1,23 @@
 import menuJson from "../menu/menu.json";
 import { IMenuData } from "../../models/IMenuData";
 import { MenuCategory } from "../menuCategory/menuCategory";
-
+import './FoodMenu.scss'
 const menuData: IMenuData = menuJson;
 
 export const FoodMenu = () => {
   // console.log(menuData);
   return (
     <>
-      {menuData.categories.map((category, index) => (
-        <MenuCategory
-          key={index}
-          title={category.title}
-          items={category.items}
-          images={category.images}
-        />
-      ))}
+      <div className="foodMenu">
+        {menuData.categories.map((category, index) => (
+          <MenuCategory
+            key={index}
+            title={category.title}
+            items={category.items}
+            images={category.images}
+          />
+        ))}
+      </div>
     </>
   );
 };
