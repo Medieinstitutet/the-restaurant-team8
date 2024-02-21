0c0c3168bf2df148dab38ee3bc33293524fccd50
maijanp <maijapapazi@gmail.com>
2024-02-21 01:55:43 +0100

    navbar made responsive

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import logo from "../../images/logo.png";
 import "./Navbar.scss";
 import { HamburgerMenu } from "../hamburgerMenu/HamburgerMenu";
 import { useState } from "react";
+import { NavLink } from "react-router-dom";
 export const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -10,11 +11,20 @@ export const Navbar = () => {
   };
   return (
     <nav>
-      <div className="logoContainer">
+      <div className="logoContainerMobile">
         <img src={logo} alt="logotyp" />
       </div>
       <div className="hamburger-menu-container">
         <HamburgerMenu isOpen={isOpen} toggleMenu={toggleMenu} />
+      </div>
+      <div className="navLinksContainerDesktop">
+        <NavLink className="navlinkDesktop" to="/">Home</NavLink>
+        <NavLink className="navlinkDesktop" to="/booking">Boka</NavLink>
+        <div className="logoContainerDesktop">
+        <img src={logo} alt="logotyp" />
+      </div>
+        <NavLink className="navlinkDesktop" to="/menu">Meny</NavLink>
+        <NavLink className="navlinkDesktop" to="/contact">Kontakt</NavLink>
       </div>
     </nav>
   );
